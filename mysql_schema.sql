e4578dbbfefe36c4f5d728862dbe46035d3581ec
Technine1 <shaimukhametov.shamil@gmail.com>
2025-03-31 17:31:13 +0400

    Delete mysql_schema.sql

--- mysql_schema.sql
+++
@@ -1,6 +0,0 @@
--- Скрипт создания таблиц для MySQL
-CREATE TABLE Departments (
-    DepartmentID INT PRIMARY KEY,
-    DepartmentName VARCHAR(100) NOT NULL
-);
--- Остальная часть структуры см. в описании выше
