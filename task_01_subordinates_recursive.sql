323b8bc9ed341b3dac0f1db99fccab51acd2eb5e
Technine1 <shaimukhametov.shamil@gmail.com>
2025-03-31 17:12:28 +0400

    Update task_01_subordinates_recursive.sql

--- task_01_subordinates_recursive.sql
+++ task_01_subordinates_recursive.sql
@@ -1,2 +1,11 @@
--- Задача 1: Рекурсивный поиск всех подчинённых Ивана Иванова
--- WITH RECURSIVE ...
+Задача 1
+Условие
+Найдите производителей (maker) и модели всех мотоциклов, которые имеют мощность более 150 лошадиных сил, стоят менее 20 тысяч долларов и являются спортивными (тип Sport). Также отсортируйте результаты по мощности в порядке убывания.
+Решение
+SELECT v.maker, m.model
+FROM Motorcycle m
+JOIN Vehicle v ON m.model = v.model
+WHERE m.horsepower > 150
+  AND m.price < 20000
+  AND m.type = 'Sport'
+ORDER BY m.horsepower DESC;
